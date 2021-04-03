5a8364336d7765be6afba60cf412c99d9810dd50
Shinya Sasaki <shinya_sasaki@precs.co.jp>
2021-04-03 17:58:52 +0900

    Forked and renamed from the phpenv project.

--- completions/mammoth.bash
+++ completions/mammoth.bash
@@ -1,16 +1,16 @@
-_phpenv() {
+_mammoth() {
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
 
   if [ "$COMP_CWORD" -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "$(phpenv commands)" -- "$word") )
+    COMPREPLY=( $(compgen -W "$(mammoth commands)" -- "$word") )
   else
     local words=("${COMP_WORDS[@]}")
     unset words[0]
     unset words[$COMP_CWORD]
-    local completions=$(phpenv completions "${words[@]}")
+    local completions=$(mammoth completions "${words[@]}")
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
   fi
 }
 
-complete -F _phpenv phpenv
+complete -F _mammoth mammoth
